139918cdf5b2e48af97bfaa0f5aa6fc34434df0a
Sam Newman <sam.newman@gmail.com>
2009-09-19 12:28:10 +0100

    Added a working method

--- src/org/magpiebrain/bigvisiblewall/buildwall/Build.scala
+++ src/org/magpiebrain/bigvisiblewall/buildwall/Build.scala
@@ -32,6 +32,8 @@ class Build(val name: String, private val status: BuildStatus, val urlToBuild: O
     return status
   }
 
+  def working : Boolean = currentActivity.equals("Sleeping")
+
   override def toString(): String = {
     return "Name:" + name + " Status:" + status + " URL:" + urlToBuild
   }
